58f2dae49468d42d13e56606ebe18640098c8bd9
ChaosLee <chaos2lee@qq.com>
2021-02-08 17:21:51 +0800

    [FIXED] point menu

--- packages/czbank-rusher-workbench/src/page/Workbench/Point/Framework.vue
+++ packages/czbank-rusher-workbench/src/page/Workbench/Point/Framework.vue
@@ -3,23 +3,60 @@
 <b-container
 	class="mt-3"
 >
+	<b-breadcrumb>
+    <b-breadcrumb-item
+			:to="{ name: 'home' }"
+		>{{ $t('u.home') }}</b-breadcrumb-item>
+    <b-breadcrumb-item
+			active
+		>{{ $t('m.point.label') }}</b-breadcrumb-item>
+	</b-breadcrumb>
+
 	<b-row>
 		<b-col
 			xxl="2"
 			xl="3"
 		>
-			<b-list-group>
-				<b-list-group-item
-					:to="{ name: 'workbench.point.history' }"
-				>{{ $t('menu.point.history.label') }}</b-list-group-item>
-				<b-list-group-item
-					:to="{ name: 'workbench.point.overview' }"
-				>{{ $t('menu.point.overview.label') }}</b-list-group-item>
-				<b-list-group-item
-					:to="{ name: 'workbench.point.adjustment' }"
-				>{{ $t('menu.point.adjustment.label') }}</b-list-group-item>
-			</b-list-group>
+			<b-card
+				no-body
+				header="总览"
+				class="mb-3"
+			>
+				<b-list-group flush>
+					<b-list-group-item
+						:to="{ name: 'workbench.point.history' }"
+					>{{ $t('menu.point.history.label') }}</b-list-group-item>
+					<b-list-group-item
+						:to="{ name: 'workbench.point.overview' }"
+					>{{ $t('menu.point.overview.label') }}</b-list-group-item>
+				</b-list-group>
+			</b-card>
+
+			<b-card
+				no-body
+				header="调整工具"
+				class="mb-3"
+			>
+				<b-list-group flush>
+					<b-list-group-item
+						:to="{ name: 'workbench.manager.overview' }"
+					>{{ $t('menu.point.adjustment.plan.label') }}</b-list-group-item>
+					<b-list-group-item
+						:to="{ name: 'workbench.manager.contribution' }"
+					>{{ $t('menu.point.adjustment.planVariation.label') }}</b-list-group-item>
+					<b-list-group-item
+						:to="{ name: 'workbench.manager.contribution' }"
+					>{{ $t('menu.point.adjustment.present.label') }}</b-list-group-item>
+					<b-list-group-item
+						:to="{ name: 'workbench.manager.contribution' }"
+					>{{ $t('menu.point.adjustment.activity.label') }}</b-list-group-item>
+					<b-list-group-item
+						:to="{ name: 'workbench.point.adjustment' }"
+					>{{ $t('menu.point.adjustment.manual.label') }}</b-list-group-item>
+				</b-list-group>
+			</b-card>
 		</b-col>
+
 		<b-col
 			xxl="10"
 			xl="9"
