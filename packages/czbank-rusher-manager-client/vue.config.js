79806279e548c0070f63f8eeb1a337b99bd4fd07
ChaosLee <chaos2lee@qq.com>
2021-02-19 15:38:37 +0800

    [ADDED] czbank-rusher-manager-client

---
+++ packages/czbank-rusher-manager-client/vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	configureWebpack: {
+		module: {
+			rules: [
+				{
+					test: /\.ya?ml$/,
+					type: 'json',
+					use: 'yaml-loader'
+				}
+			]
+		},
+		devServer: {
+      port: 8000,
+			proxy: {
+				'/api': {
+					target: 'http://127.0.0.1:3000'
+				}
+			}
+		}
+	}
+}
